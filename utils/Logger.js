57344c0af34f8e459b2903724892411fce5d8672
Boyang Niu <i-boyang@edmodo.com>
2014-12-30 21:00:28 -0500

    feat(infrastructure): add app boilerplate, react, webpack, express, logger

---
+++ utils/Logger.js
@@ -0,0 +1,23 @@
+function Logger(){
+	return this;
+}
+
+log_f = function(message, level){
+	if(!level){
+		level = "INFO";
+	}
+	console.log("[%s] %s", level, message);
+};
+
+Logger.prototype.err = function(message){
+	log_f("ERROR", message);
+}
+
+Logger.prototype.debug = function(message){
+	log_f("DEBUG", message);
+}
+
+Logger.prototype.log = log_f;
+
+
+module.exports = new Logger();
