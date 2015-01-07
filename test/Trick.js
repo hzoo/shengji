387eec96b0dd57d45cf84c9e85a84ce6b1cb8d0f
Henry Zhu <hi@henryzoo.com>
2015-01-06 21:26:36 -0500

    style(*): use dot notation instead of brackets

--- test/Trick.js
+++ test/Trick.js
@@ -38,8 +38,8 @@ describe('Trick', function(suite) {
 
 		var trick = new Trick(plays[0], 2);
 		t.equals(trick.leader, 1, 'Sets the current leader to be the leading player');
-		t.equals(trick.leadingPlay, plays[0]['cards'], 'Sets the current leading play to be the first cards played');
-		t.equals(trick.cards, plays[0]['cards'], 'Sets the set of cards played to be the first cards played');
+		t.equals(trick.leadingPlay, plays[0].cards, 'Sets the current leading play to be the first cards played');
+		t.equals(trick.cards, plays[0].cards, 'Sets the set of cards played to be the first cards played');
 		t.equals(trick.leadingSuit, ShengJi.cardSuit.DIAMONDS, 'Sets the leading suit correctly');
 
 		trick = new Trick(plays[0], ShengJi.cardValue.KING, ShengJi.cardSuit.SPADES);
@@ -57,7 +57,7 @@ describe('Trick', function(suite) {
 		t.equals(points.points(), 10, 'Calculates Kings as 10 points');
 		t.equals(nopoints.points(), 0, 'Calculates 3s as 0 points');
 
-		t.equals(points.pointCards().value, plays[0]['cards'].value, 'Returns the cards with points correctly when there are points');
+		t.equals(points.pointCards().value, plays[0].cards.value, 'Returns the cards with points correctly when there are points');
 		t.equals(nopoints.pointCards().length, 0, 'Returns the cards with points correctly when there are no points');
 	});
 
