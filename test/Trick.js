6563827a0cccdc3bbe9feb7fcaadff2b343a5901
Henry Zhu <hi@henryzoo.com>
2015-01-06 21:02:49 -0500

    refactor(leadSuit): use leadingSuit

--- test/Trick.js
+++ test/Trick.js
@@ -40,12 +40,13 @@ describe('Trick', function(suite) {
 		t.equals(trick.leader, 1, 'Sets the current leader to be the leading player');
 		t.equals(trick.leadingPlay, plays[0]['cards'], 'Sets the current leading play to be the first cards played');
 		t.equals(trick.cards, plays[0]['cards'], 'Sets the set of cards played to be the first cards played');
-		t.equals(trick.leadSuit, ShengJi.cardSuit.DIAMONDS, 'Sets the leading suit correctly');
+		t.equals(trick.leadingSuit, ShengJi.cardSuit.DIAMONDS, 'Sets the leading suit correctly');
 
 		trick = new Trick(plays[0], ShengJi.cardValue.KING, ShengJi.cardSuit.SPADES);
-		t.equals(trick.leadSuit, ShengJi.cardSuit.SPADES, 'Sets the lead suit to trump when level value card is played');
+		t.equals(trick.leadingSuit, ShengJi.cardSuit.SPADES, 'Sets the lead suit to trump when level value card is played');
+
 		trick = new Trick(joker, ShengJi.cardValue.TWO, ShengJi.cardSuit.DIAMONDS);
-		t.equals(trick.leadSuit, ShengJi.cardSuit.DIAMONDS, 'Sets the lead suit to trump when joker is played');
+		t.equals(trick.leadingSuit, ShengJi.cardSuit.DIAMONDS, 'Sets the lead suit to trump when joker is played');
 	});
 
 	it('Calculates point cards in the trick correctly', function(t) {
@@ -55,6 +56,7 @@ describe('Trick', function(suite) {
 		var nopoints = new Trick(plays[1], 2);
 		t.equals(points.points(), 10, 'Calculates Kings as 10 points');
 		t.equals(nopoints.points(), 0, 'Calculates 3s as 0 points');
+
 		t.equals(points.pointCards().value, plays[0]['cards'].value, 'Returns the cards with points correctly when there are points');
 		t.equals(nopoints.pointCards().length, 0, 'Returns the cards with points correctly when there are no points');
 	});
